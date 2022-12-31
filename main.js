5a88c8889ee8f4866f71fe4c8e105efefd7308f7
Bogdan Langhe <bogdanlanghe@gmail.com>
2022-12-31 07:55:24 +0200

    add show more /show less btn

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-  $("#show-more-button").click(function() {
-    $("#hidden-content").slideToggle();
-    $(this).find("svg").css("transform", "rotate(180deg)");
-    $(this).text(function(i, text){
-      return text === "Show Less" ? "Show More" : "Show Less";
-    });
+$(document).ready(function () {
+  $(".visibleSpan").click(function () {
+    $("#hidden-content").show();
+    $(".hiddenSpan").show();
+    $(".visibleSpan").hide();
+  });
+  $(".hiddenSpan").click(function () {
+    $("#hidden-content").hide();
+    $(".hiddenSpan").hide();
+    $(".visibleSpan").show();
   });
 });
